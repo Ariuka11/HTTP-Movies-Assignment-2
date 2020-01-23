8979add92b9e0a4b2a0463367fa8018ad6df7ba4
Ariunbold <ariun1118@gmail.com>
2020-01-23 13:54:08 -0500

    MVP done

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -17,21 +18,31 @@ export default class MovieList extends Component {
       .catch(err => console.log(err.response));
   }
 
+ 
+
   render() {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+          <MovieDetails key={movie.id} movie={movie}  />
         ))}
       </div>
     );
   }
 }
 
-function MovieDetails({ movie }) {
+function MovieDetails({movie, }) {
+
+
   return (
-    <Link to={`/movies/${movie.id}`}>
-      <MovieCard movie={movie} />
-    </Link>
+    <div>
+      <Link to={`/movies/${movie.id}`}>
+        <MovieCard movie={movie} />
+      </Link>
+      <Link to ={`/update-movie/${movie.id}`}>
+        <button className='edit-button'>Edit</button>
+      </Link>
+      
+    </div>
   );
 }
